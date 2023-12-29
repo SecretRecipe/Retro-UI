d063d7760c28ce10098cf936355f300e46824a79
Murilo Soares <murilosoliveira18@gmail.com>
2023-12-28 21:36:59 -0300

    Window hovering with cursor

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const button = document.getElementById('Windows-button');
     const menu = document.getElementsByClassName('Windows-menu');
-    
-    /* PAINT */ 
-    const paint = document.getElementById('Paint');
-    const paintWindow = document.getElementById('Paint-window');
-    paintWindow.style.display = 'none';
-    let paintClick = false
-    /* PAINT */ 
 
     menu[0].style.display = 'block';
     buttonClick = false
@@ -57,9 +50,19 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 
+    /* APPS AREA */
+
+    // PAINT
+    const paint = document.getElementById('Paint');
+    const paintWindow = document.getElementById('Paint-window');
+    const paintHeader = document.getElementById('Paint-header');
+    paintWindow.style.display = 'none';
+    let paintClick = false
+
     paint.addEventListener('click', () => {
         if (paintClick === false) {
             showWindow(paintWindow)
+            dragElement(paintHeader, paintWindow)
             paintClick = true
         }
         else {
@@ -67,7 +70,12 @@ document.addEventListener('DOMContentLoaded', () => {
             paintClick = false
         }
     })
+   
+    
 
+    
+
+    /* FUNCTIONS */
 
     function hideWindow(element, button) {
         element.style.display = 'none'
@@ -76,5 +84,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function showWindow(element) {
         element.style.display = 'block'
+    }
+
+    function dragElement(headerName, windowName) {
+        
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        headerName.onmousedown = dragMouseDown;
+
+      
+        function dragMouseDown(e) {
+          e.preventDefault();
+          pos3 = e.clientX;
+          pos4 = e.clientY;
+          document.onmouseup = closeDragElement;
+          document.onmousemove = elementDrag;
+        }
+      
+        function elementDrag(e) {
+          e = e || window.event;
+          e.preventDefault();
+          pos1 = pos3 - e.clientX;
+          pos2 = pos4 - e.clientY;
+          pos3 = e.clientX;
+          pos4 = e.clientY;
+  
+          windowName.style.top = (windowName.offsetTop - pos2) + "px";
+          windowName.style.left = (windowName.offsetLeft - pos1) + "px";
+        }
+      
+        function closeDragElement() {
+      
+          document.onmouseup = null;
+          document.onmousemove = null;
+        }
     }
 })
