15a0aa0808d07d310cfbdca3210ab7862ac56c46
murilosoarez <murilosoliveira18@gmail.com>
2024-02-04 16:08:05 -0400

    [Add intermediary window movement]

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    function getAttribute(element, style) {
-        return window.getComputedStyle(element).getPropertyValue(style)
-    }
-
-    function activateWindowDynamic(window, e) {
-        e.preventDefault()
-        console.log(e.clientX, e.clientY)
-        return window.MousePosition(e.clientX, e.clientY)
-    }
+    /* TO DO 
     
-    function moveWindow(window, left, top) {
-        window.style.left = left + 'px'
-        window.style.top = top + 'px'
-    }
+    MAKE WINDOW MOVABLE THROUGH HEADER CLICKING
+
+    -> Get clicking event working over window
+    */
 
     class WindowManager {
 
-        constructor(width, height, leftPosition, topPosition, header) {
+        constructor(width, height, leftPosition, topPosition) {
             this.width = width
             this.height = height
-            this.leftPosition = leftPosition
+            this.left = leftPosition
             this.topPosition = topPosition
-            this.header = header
         }
         /*
         MousePosition(positionX, positionY) {
@@ -62,68 +53,69 @@ document.addEventListener('DOMContentLoaded', () => {
             openMenu -= 1
         }
     })
-
+    
     let menuPrograms = Array.from(document.querySelectorAll('session'))
-    let windowsOpened = []
-
+    var openWindows = []
     menuPrograms.forEach((menuCell) => {
-        
-        menuCell.addEventListener('click', () => {
-            
-            createNewWindow(desktop, menuCell)
-            
-            let newWindow = document.getElementById('new-window')
-        
-            let openWindow = new WindowManager(getAttribute(newWindow, 'width'), getAttribute(newWindow, 'height'), newWindow.offsetLeft, newWindow.offsetTop, newWindow.childNodes[0])
 
-            windowsOpened.push(openWindow)
-            
-            
-            windowsOpened.forEach((window) => {
-                console.log("let me in")
-                window.addEventListener('click', () => {
-                    console.log('sla!')
+        menuCell.addEventListener('click', (e) => {    
+
+            createNewWindow(desktop, menuCell)
+            openWindow = document.getElementById('new-window')
+
+            openWindows.push(new WindowManager(getAttribute(openWindow, 'width'), getAttribute(openWindow, 'height')))
+
+            openWindow.firstChild.addEventListener('mouse down', function(e) {
+                let offsetY = e.clientY 
+                let offsetX = e.clientX
+                openWindow.firstChild.addEventListener('mousemove', (e) => {
+                    let newOffsetX = e.clientX - offsetX 
+                    let newOffsetY = e.clientY - offsetY
+                    openWindow.style.left = newOffsetX + 'px'
+                    openWindow.style.top = newOffsetY + 'px'
+                    
                 })
             })
-            
+       
         })
     })
-    
-
 })
 
 function createNewWindow(finalLocation, cellClicked) {
 
     if (cellClicked.className.includes('Closed')) {
-
+        
         let newWindow = document.createElement('div')
         newWindow.id = 'new-window'
         newWindow.className = 'New Window'
-
+        
         let windowHeader = document.createElement('div')
         windowHeader.style.width = '100%'
         windowHeader.style.height = '10%'
         windowHeader.style.background = 'lightblue'
         windowHeader.id = 'window-header'
-
+        
         let windowContent = document.createElement('object')
         windowContent.style.width = '100%'
         windowContent.style.height = '100%'
-
+        
         if (cellClicked.className == 'Paint Cell') {
             windowContent.setAttribute('data', 'pages/paint/paint.html')
         }
-
+        
         newWindow.append(windowHeader)
         newWindow.append(windowContent)
         finalLocation.append(newWindow)
         
         newClassName = cellClicked.className.replace('Closed', 'Open')
         cellClicked.className = newClassName
+        
+    }   
+}
 
-    }
-
-
+function activateWindowDynamic(window, e) {
+    e.preventDefault()
+    return window.MousePosition(e.clientX, e.clientY)
 }
 
 function getAttribute(element, style) {
