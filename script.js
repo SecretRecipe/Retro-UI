a23bf309753ee8cbc6ee4e2e166b0f63bf0ee08e
murilosoarez <murilosoliveira18@gmail.com>
2024-02-09 06:22:24 -0400

    [Update menu prototype design]

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
             openWindows =  Array.from(document.getElementsByClassName('New Window'))
 
             moveWindowEvent(openWindows)
+            resizableWindowEvent(openWindows)
        
         })
     })
@@ -100,9 +101,14 @@ function createNewWindow(finalLocation, cellClicked) {
         windowContent.style.width = '100%'
         windowContent.style.height = '100%'
         
+      
+
+        
+
         if (cellClicked.className == 'Paint Cell') {
             windowContent.setAttribute('data', 'pages/paint/paint.html')
         }
+        
         
         newWindow.append(windowHeader)
         newWindow.append(windowContent)
@@ -114,20 +120,19 @@ function createNewWindow(finalLocation, cellClicked) {
     }   
 }
 
-function activateWindowDynamic(window, e) {
-    e.preventDefault()
-    return window.MousePosition(e.clientX, e.clientY)
+function getAttribute(element, style) {
+    return window.getComputedStyle(element).getPropertyValue(style)
 }
 
 function moveWindowEvent(window) {
-
     for (i=0; i < window.length; i++) {
         (function (currentWindow) {
             currentWindow.firstChild.addEventListener('mousedown', (e) => {
-            
+                
+
                 let offsetX = e.offsetX
                 let offsetY = e.offsetY
-                console.log(offsetX, offsetY)
+                
                 
                 currentWindow.firstChild.addEventListener('mousemove', newOffset)
                 currentWindow.firstChild.addEventListener('mouseup', removingEvents)
@@ -153,9 +158,171 @@ function moveWindowEvent(window) {
     }
 }
 
-function getAttribute(element, style) {
-    return window.getComputedStyle(element).getPropertyValue(style)
+function resizableWindowEvent(window) {
+    for (i=0; i < window.length; i++) {
+        (function (tackledWindow, size= 10) {
+            
+            const top = document.createElement('div')
+            top.style.width = '100%';
+            top.style.height = size + 'px';
+            top.style.backgroundColor = 'transparent';
+            top.style.position = 'absolute';
+            top.style.top = - (size / 2) + 'px';
+            top.style.left = '0px';
+            top.style.cursor = 'n-resize';
+            top.id = 'top'
+
+            const bottom = document.createElement('div');
+            bottom.style.width = '100%';
+            bottom.style.height = size + 'px';
+            bottom.style.backgroundColor = 'yellow';
+            bottom.style.position = 'absolute';
+            bottom.style.bottom = - (size / 2) + 'px';
+            bottom.style.left = '0px';
+            bottom.style.cursor = 'n-resize';
+            bottom.id = 'bottom'
+            
+            const left = document.createElement('div');
+            left.style.width = size + 'px';
+            left.style.height = '100%';
+            left.style.backgroundColor = 'transparent';
+            left.style.position = 'absolute';
+            left.style.top = '0px';
+            left.style.left = - (size / 2) + 'px';
+            left.style.cursor = 'e-resize';
+            left.id = 'left'
+            
+            const right = document.createElement('div');
+            right.style.width = size + 'px';
+            right.style.height = '100%';
+            right.style.backgroundColor = 'transparent';
+            right.style.position = 'absolute';
+            right.style.top = '0px';
+            right.style.right = - (size / 2) + 'px';
+            right.style.cursor = 'e-resize';
+            right.id = 'right'
+
+            const corner1 = document.createElement('div');
+            corner1.style.width = size + 'px';
+            corner1.style.height = size + 'px';
+            corner1.style.backgroundColor = 'transparent';
+            corner1.style.position = 'absolute';
+            corner1.style.top = - (size / 2) + 'px';
+            corner1.style.left = - (size / 2) + 'px';
+            corner1.style.cursor = 'nw-resize';
+            
+            const corner2 = document.createElement('div');
+            corner2.style.width = size + 'px';
+            corner2.style.height = size + 'px';
+            corner2.style.backgroundColor = 'transparent';
+            corner2.style.position = 'absolute';
+            corner2.style.top = - (size / 2) + 'px';
+            corner2.style.right = - (size / 2) + 'px';
+            corner2.style.cursor = 'ne-resize';
+            
+            const corner3 = document.createElement('div');
+            corner3.style.width = size + 'px';
+            corner3.style.height = size + 'px';
+            corner3.style.backgroundColor = 'transparent';
+            corner3.style.position = 'absolute';
+            corner3.style.bottom = - (size / 2) + 'px';
+            corner3.style.left = - (size / 2) + 'px';
+            corner3.style.cursor = 'sw-resize';
+            
+            const corner4 = document.createElement('div');
+            corner4.style.width = size + 'px';
+            corner4.style.height = size + 'px';
+            corner4.style.backgroundColor = 'transparent';
+            corner4.style.position = 'absolute';
+            corner4.style.bottom = - (size / 2) + 'px';
+            corner4.style.right = - (size / 2) + 'px';
+            corner4.style.cursor = 'se-resize';
+
+            tackledWindow.append(top)
+            tackledWindow.append(bottom)
+            tackledWindow.append(left)
+            tackledWindow.append(right)
+            tackledWindow.appendChild(corner1);
+            tackledWindow.appendChild(corner2);
+            tackledWindow.appendChild(corner3);
+            tackledWindow.appendChild(corner4);
+
+
+
+            /*
+            corner1.addEventListener('mousedown', resizeDiagonal_one);
+            corner2.addEventListener('mousedown', resizeDiagonal_two);
+            corner3.addEventListener('mousedown', resizeDiagonal_three);
+            corner4.addEventListener('mousedown', resizeDiagonal_four);
+            */
+            top.addEventListener('mousedown', resizeWindow);
+            left.addEventListener('mousedown', resizeWindow);
+            right.addEventListener('mousedown', resizeWindow);
+            /*
+            bottom.addEventListener('mousedown', resizeYPositive);
+            */
+
+            var windowsHeight = parseInt((getAttribute(tackledWindow, 'height'))) 
+            var windowsWidth = parseInt((getAttribute(tackledWindow, 'width')))
+            var offsetTop = tackledWindow.offsetTop
+            var offsetLeft = tackledWindow.offsetLeft
+
+            function resizeWindow(e) {
+
+                let offsetX = e.clientX
+                let offsetY = e.clientY
+
+                let tackledBorder = document.getElementById(this.id).id
+                tackledWindow.addEventListener('mousemove', startDrag)
+                tackledWindow.addEventListener('mouseup', stopDragging)
+                
+                function startDrag(e) {
+                
+                    if (tackledBorder == 'top') {
+                      
+                    }
+                    
+                    else if (tackledBorder == 'left') {
+                        
+                    }
+
+                    else if (tackledBorder == 'right') {
+                     
+                    }
+
+                    else if (tackledBorder == 'bottom') {
+                    
+                    }
+
+                    else if (tackledBorder == 'corner1') {
+                    
+                    }
+
+                    else if (tackledBorder == 'corner2') {
+                    
+                    }
+
+                    else if (tackledBorder == 'corner3') {
+                    
+                    }
+
+                    else if (tackledBorder == 'corner4') {
+                    
+                    }
+                }
+
+                function stopDragging() {
+                    tackledWindow.removeEventListener('mousemove', startDrag);
+                }
+
+            }
+
+        }
+        (window[i]))
+    }
 }
+
+
 
 
 
