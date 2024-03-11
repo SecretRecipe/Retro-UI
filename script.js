6fe4e86d7e84d39b27356fc75e3330f0f380159d
murilosoarez <murilosoliveira18@gmail.com>
2024-03-11 08:10:58 -0400

    [Update Internet Explorer design]

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ import { openWindow } from './modules/createWindow.js'
 document.addEventListener('DOMContentLoaded', () => {
 
     let menuApps = Array.from(document.getElementsByClassName('Cell Closed'))
-    let body = document.querySelector('body')
 
     menuApps.forEach((app) => {
         
+        let backgroundColor = getAttribute(app, 'background-color')
+
         app.addEventListener('mouseenter', () => {
             app.style.background = 'rgb(78, 129, 231)'
             app.style.color = 'white'
@@ -15,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         
         app.addEventListener('mouseleave', () => {
             app.style.color = 'black'
+            app.style.background = backgroundColor
         })
         
         app.addEventListener('click', () => {
@@ -25,3 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
     
 })
 
+function getAttribute(element, style) {
+    return window.getComputedStyle(element).getPropertyValue(style)
+}
