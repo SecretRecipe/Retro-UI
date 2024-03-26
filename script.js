475751187451e187b5a2c7e18e3f05ec1df28984
murilosoarez <murilosoliveira18@gmail.com>
2024-03-26 10:59:35 -0400

    [Organizing main script]

--- script.js
+++ script.js
@@ -23,88 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
         
         app.addEventListener('click', () => {
             openWindow(app, footer)
-            addEvents(app)
         })
         
     })
 
-    function addEvents(app) {
-   
-        let window = document.getElementById('Window' + ' ' + app.id)
-        let header = document.getElementById('Header' + ' ' + app.id)
-        let footer = document.getElementById('footer')
-        let borders = Array.from(document.getElementsByClassName('Window Border'))
-        //let buttons = Array.from(document.getElementsByClassName('Window Buttons'))
-
-        //header.addEventListener('mousedown', getDragEvents)
-        
-        //borders.forEach((border) => border.addEventListener('mousedown', getDragEvents))
-        /*
-        function getDragEvents(e) {
-          
-            let X = e.clientX // Gets initial X offset clicking
-            let Y = e.clientY // Gets initial Y offset clicking
-            let left = window.offsetLeft // Gets left actual distance
-            let top = window.offsetTop // Gets top actual distance
-            let height = parseInt(getAttribute(window, 'height'))
-            let width = parseInt(getAttribute(window, 'width'))
-
-            if (e.target.id.includes('Header')) {
-                
-                document.addEventListener('mousemove', moveWindow)
-                document.addEventListener('mouseup', stopEvent)
-
-                function moveWindow(e) {
-                    let offsetX = e.clientX - X
-                    let offsetY = e.clientY - Y
-                    window.style.left = offsetX + left + 'px'
-                    window.style.top =  offsetY + top + 'px'
-                    
-                }
-
-                function stopEvent() {
-                    document.removeEventListener('mousemove', moveWindow)
-                }
-                
-            }
-            
-            if (e.target.className.includes('Border')) {
-                
-                let tackledBorder = e.target.id
-                document.addEventListener('mousemove', resizeWindow)
-                document.addEventListener('mouseup', stopEvent)
-
-                function resizeWindow(e) {
-
-                    if (tackledBorder == 'top'  || tackledBorder == 'corner-1' || tackledBorder == 'corner-2') {
-                        window.style.top = (top + (e.clientY - Y)) + 'px'
-                        window.style.height = (height - (e.clientY - Y)) + 'px'
-                    }
-
-                    if (tackledBorder == 'left' || tackledBorder == 'corner-1' || tackledBorder == 'corner-3') {
-                        window.style.left = (left + (e.clientX - X)) + 'px'
-                        window.style.width = (width - (e.clientX - X)) + 'px'
-                    }
-                    
-                    if (tackledBorder == 'bottom' || tackledBorder == 'corner-3' || tackledBorder == 'corner-4') { 
-                        window.style.height = (height - (Y - e.clientY)) + 'px'     
-                    }
-
-                    if (tackledBorder == 'right' || tackledBorder == 'corner-2' || tackledBorder == 'corner-4') {
-                        window.style.width = (width - (X - e.clientX)) + 'px'         
-                    }
-                }
-
-                function stopEvent() {
-                    document.removeEventListener('mousemove', resizeWindow)
-                }
-            }
-            
-      
-    
-        }
-        */
-        
-    }
 })
 
